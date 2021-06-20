5ba71f215b32f6c2bda369a5ccc1ef27e6c1af1c
anhlne <nawilldominate@gmail.com>
2021-06-20 09:38:34 +0700

    Update router

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const configs = require('./config/index.js');
 const userRouter = require('./modules/users/user.router');
+const betRouter = require('./modules/bets/bet.router');
+const rollRouter = require('./modules/rolls/roll.router');
 
 mongoose.connect(configs.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
+// API routers
 app.use('/api/users', userRouter.router);
+app.use('/api/bets', userRouter.router);
+app.use('/api/rolls', userRouter.router);
+
+// Static file router
 app.use('/', express.static(__dirname + '/views'));
 
 app.listen(configs.PORT, function() {
