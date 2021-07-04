b8e37ba252a304e8fffc816656e27d210fe6ca20
anhlne <nawilldominate@gmail.com>
2021-07-04 20:00:46 +0700

    Update config

--- config/index.js
+++ config/index.js
@@ -1,10 +1,6 @@
 module.exports = {
     MONGO_CONNECTION_URL: 'mongodb://localhost:27017/bautomcuaca',
     PORT: 3000,
-<<<<<<< HEAD
-    ACCESS_TOKEN_SECRET: 'bau_cua_tom_ca_secret_token'
-=======
     ACCESS_TOKEN_SECRET: 'access-token-secret-example-bau-cua-tom-ca',
     ACCESS_TOKEN_LIFE: '1h',
->>>>>>> origin/feature/api/login
 };
